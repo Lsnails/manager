2abcd4bb372166cf8f2b0e6acfee7bfd7394b51c
wanghuan <wanghuan@chinasofti.com>
2019-10-15 14:47:18 +0800

    微信user表加5个备用字段
    Change-Id: I8055935b1f41cec9f9eeb9c1834be36c05f9ba80
    Signed-off-by: wanghuan <wanghuan@chinasofti.com>

--- admin/src/main/java/com/base/modules/business/system/wxuser/entity/WxUserEntity.java
+++ admin/src/main/java/com/base/modules/business/system/wxuser/entity/WxUserEntity.java
@@ -60,6 +60,12 @@ public class WxUserEntity implements Serializable {
 	 * 
 	 */
 	private Date updateDate;
+	
+	private String remark;
+	private String remark2;
+	private String remark3;
+	private String remark4;
+	private String remark5;
 
 	/**
 	 * 设置：
@@ -181,4 +187,35 @@ public class WxUserEntity implements Serializable {
 	public Date getUpdateDate() {
 		return updateDate;
 	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public String getRemark2() {
+		return remark2;
+	}
+	public void setRemark2(String remark2) {
+		this.remark2 = remark2;
+	}
+	public String getRemark3() {
+		return remark3;
+	}
+	public void setRemark3(String remark3) {
+		this.remark3 = remark3;
+	}
+	public String getRemark4() {
+		return remark4;
+	}
+	public void setRemark4(String remark4) {
+		this.remark4 = remark4;
+	}
+	public String getRemark5() {
+		return remark5;
+	}
+	public void setRemark5(String remark5) {
+		this.remark5 = remark5;
+	}
+	
 }
