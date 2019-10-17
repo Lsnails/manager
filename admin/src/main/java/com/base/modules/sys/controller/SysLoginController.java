d7d3cc431d8d31bf3ad2dec2ba00ec7329692fd5
weizechao <zechao.wei@aiyingli.com>
2019-10-17 16:21:58 +0800

    判断请求是来自pc还是mobile

--- admin/src/main/java/com/base/modules/sys/controller/SysLoginController.java
+++ admin/src/main/java/com/base/modules/sys/controller/SysLoginController.java
@@ -1,22 +1,22 @@
 package com.base.modules.sys.controller;
 
 
+import com.base.common.utils.R;
 import com.base.modules.sys.shiro.ShiroUtils;
 import com.google.code.kaptcha.Constants;
 import com.google.code.kaptcha.Producer;
-import com.base.common.utils.R;
-
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -27,6 +27,7 @@ import java.io.IOException;
  * @date 2016年11月10日 下午1:15:31
  */
 @Controller
+@Slf4j
 public class SysLoginController {
 	@Autowired
 	private Producer producer;
@@ -52,9 +53,10 @@ public class SysLoginController {
 	 */
 	@ResponseBody
 	@RequestMapping(value = "/sys/login", method = RequestMethod.POST)
-	public R login(String username, String password, String captcha) {
-		
-		//注释验证码
+	public R login(String username, String password, String captcha, HttpServletRequest request) {
+        boolean mobileDevice = JudgeIsMoblie(request);
+        log.error("浏览器标识 是否为手机端 : -->" + mobileDevice);
+        //注释验证码
 //		String kaptcha = ShiroUtils.getKaptcha(Constants.KAPTCHA_SESSION_KEY);
 //		if(!captcha.equalsIgnoreCase(kaptcha)){
 //			return R.error("验证码不正确");
@@ -73,8 +75,9 @@ public class SysLoginController {
 		}catch (AuthenticationException e) {
 			return R.error("账户验证失败");
 		}
-	    
-		return R.ok();
+        R r = new R();
+		r.put("mobile",mobileDevice);
+        return r;
 	}
 	
 	/**
@@ -102,6 +105,34 @@ public class SysLoginController {
 		//TODO Auto-generated catch block  此方法不需要迁移至网站登录代码中
 		return R.error(999, "网站未登录");
     }
- 
+
+
+    public boolean JudgeIsMoblie(HttpServletRequest request) {
+        boolean isMoblie = false;
+        String[] mobileAgents = { "iphone", "android", "phone", "mobile", "wap", "netfront", "java", "opera mobi",
+                "opera mini", "ucweb", "windows ce", "symbian", "series", "webos", "sony", "blackberry", "dopod",
+                "nokia", "samsung", "palmsource", "xda", "pieplus", "meizu", "midp", "cldc", "motorola", "foma",
+                "docomo", "up.browser", "up.link", "blazer", "helio", "hosin", "huawei", "novarra", "coolpad", "webos",
+                "techfaith", "palmsource", "alcatel", "amoi", "ktouch", "nexian", "ericsson", "philips", "sagem",
+                "wellcom", "bunjalloo", "maui", "smartphone", "iemobile", "spice", "bird", "zte-", "longcos",
+                "pantech", "gionee", "portalmmm", "jig browser", "hiptop", "benq", "haier", "^lct", "320x320",
+                "240x320", "176x220", "w3c ", "acs-", "alav", "alca", "amoi", "audi", "avan", "benq", "bird", "blac",
+                "blaz", "brew", "cell", "cldc", "cmd-", "dang", "doco", "eric", "hipt", "inno", "ipaq", "java", "jigs",
+                "kddi", "keji", "leno", "lg-c", "lg-d", "lg-g", "lge-", "maui", "maxo", "midp", "mits", "mmef", "mobi",
+                "mot-", "moto", "mwbp", "nec-", "newt", "noki", "oper", "palm", "pana", "pant", "phil", "play", "port",
+                "prox", "qwap", "sage", "sams", "sany", "sch-", "sec-", "send", "seri", "sgh-", "shar", "sie-", "siem",
+                "smal", "smar", "sony", "sph-", "symb", "t-mo", "teli", "tim-", "tosh", "tsm-", "upg1", "upsi", "vk-v",
+                "voda", "wap-", "wapa", "wapi", "wapp", "wapr", "webc", "winw", "winw", "xda", "xda-",
+                "Googlebot-Mobile" };
+        if (request.getHeader("User-Agent") != null) {
+            for (String mobileAgent : mobileAgents) {
+                if (request.getHeader("User-Agent").toLowerCase().indexOf(mobileAgent) >= 0) {
+                    isMoblie = true;
+                    break;
+                }
+            }
+        }
+        return isMoblie;
+    }
 	
 }
